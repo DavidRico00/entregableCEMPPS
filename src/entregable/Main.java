f57f5c6ada8f1bd6e9a968b5828f2bb43a3b77be
David Rico <davidriico00@gmail.com>
2024-09-26 18:14:44 +0200

    merge de rama 1 en master

--- src/entregable/Main.java
+++ src/entregable/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Primer commit");
+		
+		System.out.println("Escribo 2 en rama 1");
+		
+		System.out.println("Escribo 3 en rama 1");
 
 	}
 
